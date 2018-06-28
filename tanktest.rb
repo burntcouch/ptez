4b4c373b3578158dc8aeba5ff88f402e6a3ab00b
burntcouch <burntcouch@gmail.com>
2018-06-27 20:41:22 -0700

    Better display for tanktest.rb

--- tanktest.rb
+++ tanktest.rb
@@ -6,15 +6,17 @@ require './nukecook.rb'
 include NukeCooker
 
 e = NukeEnv.new('./data/nuclides.csv')
-t = NukeTank.new(e, 100)
+t = NukeTank.new(e, 1000)
 
 dt = 1.0
-ts = 100
+ts = 150
 
 loop do
-	newt = t.cook(ts, dt, {n: 10, T: 1})
+	newt = t.cook(ts, dt, {n: 12, T: 2, He4: 1})
 	specnt = t.tank.inject(Hash.new(0)) {|h, n| h[n.name] += 1; h}
-	specnt.each do |k, v|
+	speck = specnt.keys.sort {|a,b| (e.nukes[a][:n] + e.nukes[a][:p]) <=> (e.nukes[b][:n] + e.nukes[b][:p]) }
+	speck.each do |k|
+		v = specnt[k]
 		print "#{k.to_s} - #{v.to_s}" 
 		print " HALF: #{e.nukes[k][:half]}" unless e.nukes[k].nil?
 		print "\n"
@@ -26,7 +28,7 @@ loop do
 
 	if dummy =~ /dump/
 		puts "-----------------------------------------------------"
-		t.tank.each do |n|	
+		t.tank.sort {|a,b| (a.p + a.n) <=> (b.p + b.n)}.each do |n|	
 			puts n.to_s	
 		end
 		gets
